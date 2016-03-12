ec5115691b6556f7a461461d4d4a32df30094da8
Lkytal <lkytal@live.cn>
2016-03-12 16:57:35 +0800

    Init

---
+++ SmoothScrollMouseProcessor.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace SmoothScroll
+{
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable")]
+	internal sealed class SmoothScrollMouseProcessor : MouseProcessorBase
+	{
+		private readonly object Locker = new object();
+
+		private readonly Dispatcher DispatcherAgent;
+		private readonly IWpfTextView WpfTextView;
+
+		private double Remain = 0;
+		private int ReScroll = 0;
+
+		private Task ScrollTask = null;
+
+		internal SmoothScrollMouseProcessor(IWpfTextView wpfTextView)
+		{
+			this.DispatcherAgent = Dispatcher.CurrentDispatcher;
+			this.WpfTextView = wpfTextView;			
+		}
+
+		private static float AmountToScroll(float a, float b, float amount)
+		{
+			return a + (b - a) * amount;
+		}
+
+		public override void PreprocessMouseWheel(MouseWheelEventArgs e)
+		{
+			if (Keyboard.IsKeyDown(Key.LeftCtrl) || Keyboard.IsKeyDown(Key.RightCtrl))
+			{
+				return;
+			}
+
+			lock(Locker)
+			{
+				Remain += e.Delta * 1.2;
+				ReScroll = 1;
+			}
+
+			if (ScrollTask == null || ScrollTask.IsCompleted)
+			{
+				ScrollTask = new Task(() => this.SmoothScroll());
+				ScrollTask.Start();
+			}		
+
+			e.Handled = true;
+		}
+
+		private void Scroll(double value)
+		{
+			Action Act = () => {
+				this.WpfTextView.ViewScroller.ScrollViewportVerticallyByPixels(value);
+			};
+
+			this.DispatcherAgent.BeginInvoke(Act);
+		}
+
+		private void SmoothScroll()
+		{
+			for (int i = 0; i < 50; i++ )
+			{
+				if (Math.Abs(Remain) < 1)
+				{
+					break;
+				}
+
+				lock (Locker)
+				{
+					if (ReScroll == 1)
+					{
+						ReScroll = 0;
+						i = 0; //Restart
+					}
+
+					double Step = Remain / 10;
+					Remain -= Step;
+					Scroll(Step);
+				}
+				
+				Thread.Sleep(12);
+			}
+
+			Scroll(Remain);
+		}
+	}
+}
---
+++ SmoothScrollMouseProcessorProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace SmoothScroll
+{
+    [Export(typeof(IMouseProcessorProvider))]
+    [ContentType("text")]
+    [TextViewRole(PredefinedTextViewRoles.Interactive)]
+    [Name("Smooth Scroll Mouse Processor")]
+    internal sealed class SmoothScrollMouseProcessorProvider : IMouseProcessorProvider
+    {
+        IMouseProcessor IMouseProcessorProvider.GetAssociatedProcessor(IWpfTextView wpfTextView)
+        {
+            return new SmoothScrollMouseProcessor(wpfTextView);
+        }
+    }
+}
