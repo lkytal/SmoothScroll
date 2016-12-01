67564cf210537fdafa8b95c25385d021edaa9aea
Lkytal <lkytal@live.cn>
2016-12-01 21:27:04 +0800

    Horizontal

--- SmoothScrollMouseProcessor.cs
+++ SmoothScrollMouseProcessor.cs
@@ -10,15 +10,16 @@ namespace SmoothScroll
 	[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1001:TypesThatOwnDisposableFieldsShouldBeDisposable")]
 	internal sealed class SmoothScrollMouseProcessor : MouseProcessorBase
 	{
-		private readonly object Locker = new object();
+		private readonly object LockerVertical = new object();
+		private readonly object LockerHorizontal = new object();
 
 		private readonly Dispatcher DispatcherAgent;
 		private readonly IWpfTextView WpfTextView;
 
-		private double Remain = 0;
-		private int ReScroll = 0;
+		private double VerticalRemain = 0, HorizontalRemain = 0;
+		private int VerticalReScroll = 0, HorizontalReScroll = 0;
 
-		private Task ScrollTask = null;
+		private Task VerticalTask = null, HorizontalTask = null;
 
 		private bool ExtEnable { get; set; }
 		private bool ShiftEnable { get; set; }
@@ -36,12 +37,12 @@ namespace SmoothScroll
 
 			if (SmoothScrollPackage.OptionsPage != null)
 			{
-				ShiftEnable = SmoothScrollPackage.OptionsPage.ShiftEnable;
-				AltEnable = SmoothScrollPackage.OptionsPage.AltEnable;
-				ExtEnable = SmoothScrollPackage.OptionsPage.ExtEnable;
+				ShiftEnable  = SmoothScrollPackage.OptionsPage.ShiftEnable;
+				AltEnable    = SmoothScrollPackage.OptionsPage.AltEnable;
+				ExtEnable    = SmoothScrollPackage.OptionsPage.ExtEnable;
 				SmoothEnable = SmoothScrollPackage.OptionsPage.SmoothEnable;
-				SpeedRadio = SmoothScrollPackage.OptionsPage.SpeedRadio;
-				TimeRadio = SmoothScrollPackage.OptionsPage.TimeRadio;
+				SpeedRadio   = SmoothScrollPackage.OptionsPage.SpeedRadio;
+				TimeRadio    = SmoothScrollPackage.OptionsPage.TimeRadio;
 				steps = (int)(50 * TimeRadio);
 			}
 		}
@@ -60,7 +61,8 @@ namespace SmoothScroll
 
 			if (this.ShiftEnable && (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift)))
 			{
-				this.WpfTextView.ViewScroller.ScrollViewportHorizontallyByPixels(-e.Delta);
+				//this.WpfTextView.ViewScroller.ScrollViewportHorizontallyByPixels(-e.Delta);
+				StartScroll(e.Delta, 2);				
 
 				e.Handled = true;
 
@@ -70,6 +72,7 @@ namespace SmoothScroll
 			if (this.AltEnable && (Keyboard.IsKeyDown(Key.LeftAlt) || Keyboard.IsKeyDown(Key.RightAlt)))
 			{
 				this.WpfTextView.ViewScroller.ScrollViewportVerticallyByPage(e.Delta < 0 ? ScrollDirection.Down : ScrollDirection.Up);
+				//StartScroll(e.Delta * 4, 1);
 
 				e.Handled = true;
 
@@ -80,52 +83,111 @@ namespace SmoothScroll
 			{
 				e.Handled = true;
 
-				lock (Locker)
+				StartScroll(e.Delta, 1);
+			}
+		}
+
+		private void StartScroll(double distance, int direction)
+		{
+			if (direction == 1)
+			{
+				lock (LockerVertical)
 				{
-					Remain += e.Delta * SpeedRadio;
-					ReScroll = 1;
+					VerticalRemain += distance * SpeedRadio;
+					VerticalReScroll = 1;
 				}
 
-				if (ScrollTask == null || ScrollTask.IsCompleted)
+				if (VerticalTask == null || VerticalTask.IsCompleted)
 				{
-					ScrollTask = new Task(this.SmoothScroll);
-					ScrollTask.Start();
+					VerticalTask = new Task(this.VerticallyScrollingThread);
+					VerticalTask.Start();
+				}
+			}
+			else
+			{
+				lock (LockerHorizontal)
+				{
+					HorizontalRemain += -distance * SpeedRadio;
+					HorizontalReScroll = 1;
+				}
+
+				if (HorizontalTask == null || HorizontalTask.IsCompleted)
+				{
+					HorizontalTask = new Task(this.HorizontallyScrollingThread);
+					HorizontalTask.Start();
 				}
 			}
 		}
 
-		private void Scroll(double value)
+		private void Scroll(double value, int direction)
 		{
 			Action act = () =>
 			{
-				this.WpfTextView.ViewScroller.ScrollViewportVerticallyByPixels(value);
+				if (direction == 1)
+				{
+					this.WpfTextView.ViewScroller.ScrollViewportVerticallyByPixels(value);
+				}
+				else if (direction == 2)
+				{
+					this.WpfTextView.ViewScroller.ScrollViewportHorizontallyByPixels(value);
+				}
 			};
 
 			this.DispatcherAgent.BeginInvoke(act);
 		}
 
-		private void SmoothScroll()
+		private void HorizontallyScrollingThread()
 		{
 			for (int i = 0; i < steps; i++)
 			{
-				lock (Locker)
+				lock (LockerHorizontal)
 				{
-					if (Math.Abs(Remain) < 10)
+					double distance = AmountToScroll(HorizontalRemain, i);
+
+					if (Math.Abs(HorizontalRemain) < 15 || Math.Abs(distance) < 1)
 					{
-						Remain = 0;
+						HorizontalRemain = 0;
 						break;
 					}
 
-					if (ReScroll == 1)
+					if (HorizontalReScroll == 1)
 					{
-						ReScroll = 0;
+						HorizontalReScroll = 0;
 
 						i = 0; //Restart
 					}
 
-					double distance = AmountToScroll(Remain, i);
-					Remain -= distance;
-					Scroll(distance);
+					HorizontalRemain -= distance;
+					Scroll(distance, 2);
+				}
+
+				Thread.Sleep(15);
+			}
+		}
+
+		private void VerticallyScrollingThread()
+		{
+			for (int i = 0; i < steps; i++)
+			{
+				lock (LockerVertical)
+				{
+					double distance = AmountToScroll(VerticalRemain, i);
+
+					if (Math.Abs(VerticalRemain) < 10 || Math.Abs(distance) < 1)
+					{
+						VerticalRemain = 0;
+						break;
+					}
+
+					if (VerticalReScroll == 1)
+					{
+						VerticalReScroll = 0;
+
+						i = 0; //Restart
+					}
+
+					VerticalRemain -= distance;
+					Scroll(distance, 1);
 				}
 
 				Thread.Sleep(15);
