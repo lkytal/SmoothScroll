d307449c46c9ec005d2d6084eec0f0e6498f63c0
imReker <reker132@gmail.com>
2018-11-14 16:46:22 +0800

    Add horizontal mouse wheel support.
    Added WM_MOUSEHWHEEL mesage handler to support horizontal mouse wheel.

--- TestWpf/MainWindow.xaml.cs
+++ TestWpf/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -17,66 +18,108 @@ using ScrollShared;
 
 namespace TestWpf
 {
-	/// <summary>
-	/// MainWindow.xaml 的交互逻辑
-	/// </summary>
-	public partial class MainWindow : Window, IPageScroller
-	{
-		private ScrollController scrollController;
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window, IPageScroller
+    {
+        const int WM_MOUSEHWHEEL = 0x020E;
+        private ScrollController vscrollController;
+        private ScrollController hscrollController;
 
-		public MainWindow()
-		{
-			InitializeComponent();
-		}
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
 
-		private void Window_Loaded(object sender, RoutedEventArgs e)
-		{
-			scrollController = new ScrollController(this, ScrollingDirection.Vertical);
-			textBox.PreviewMouseWheel += textBox_PreviewMouseWheel;
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            vscrollController = new ScrollController(this, ScrollingDirection.Vertical);
+            hscrollController = new ScrollController(this, ScrollingDirection.Horizontal);
+            textBox.PreviewMouseWheel += textBox_PreviewMouseWheel;
+            HwndSource source = PresentationSource.FromVisual(textBox) as HwndSource;
+            source?.AddHook(textBox_Hook);
 
-			var fs = new FileStream(@"..\..\..\scrollController\ScrollController.cs", FileMode.Open, FileAccess.Read, FileShare.None);
-			var reader = new StreamReader(fs);
-			var text = reader.ReadToEnd();
-			reader.Close();
-			fs.Close();
+            var fs = new FileStream(@"..\..\..\scrollController\ScrollController.cs", FileMode.Open, FileAccess.Read, FileShare.None);
+            var reader = new StreamReader(fs);
+            var text = reader.ReadToEnd();
+            reader.Close();
+            fs.Close();
 
-			textBox.Text = text + text + text;
-			textBox.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
-		}
+            textBox.Text = text + text + text;
+            textBox.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+        }
 
-		public void Scroll(ScrollingDirection direction, double value)
-		{
-			Action act = () =>
-			{
-				if (direction == ScrollingDirection.Vertical)
-				{
-					textBox.ScrollToVerticalOffset(textBox.VerticalOffset - value);
-				}
-				else if (direction == ScrollingDirection.Horizontal)
-				{
-					textBox.ScrollToHorizontalOffset(textBox.HorizontalOffset - value);
-				}
-			};
+        public void Scroll(ScrollingDirection direction, double value)
+        {
+            Action act = () =>
+            {
+                if (direction == ScrollingDirection.Vertical)
+                {
+                    textBox.ScrollToVerticalOffset(textBox.VerticalOffset - value);
+                }
+                else if (direction == ScrollingDirection.Horizontal)
+                {
+                    textBox.ScrollToHorizontalOffset(textBox.HorizontalOffset - value);
+                }
+            };
 
-			this.Dispatcher.BeginInvoke(act);
-		}
+            this.Dispatcher.BeginInvoke(act);
+        }
 
-		private void textBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
-		{
-			e.Handled = true;
+        private void scroll(int delta, ScrollController controller)
+        {
+            ScrollingSpeeds scrollingSpeeds = ScrollingSpeeds.Normal;
 
-			ScrollingSpeeds scrollingSpeeds = ScrollingSpeeds.Normal;
+            if (radioSlow.IsChecked ?? false)
+            {
+                scrollingSpeeds = ScrollingSpeeds.Slow;
+            }
+            else if (radioFast.IsChecked ?? false)
+            {
+                scrollingSpeeds = ScrollingSpeeds.Fast;
+            }
 
-			if (radioSlow.IsChecked ?? false)
-			{
-				scrollingSpeeds = ScrollingSpeeds.Slow;
-			}
-			else if (radioFast.IsChecked ?? false)
-			{
-				scrollingSpeeds = ScrollingSpeeds.Fast;
-			}
+            controller.ScrollView(delta, scrollingSpeeds);
+        }
 
-			scrollController.ScrollView(e.Delta, scrollingSpeeds);
-		}
-	}
+        private void textBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+            if (Keyboard.IsKeyDown(Key.LeftShift) || Keyboard.IsKeyDown(Key.RightShift))
+            {
+                scroll(e.Delta, hscrollController);
+            } else
+            {
+                scroll(e.Delta, vscrollController);
+            }
+        }
+
+        private void textBox_OnMouseHWheel(int delta)
+        {
+            scroll(Math.Sign(delta) * -20, hscrollController);
+        }
+
+        private IntPtr textBox_Hook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch (msg)
+            {
+                case WM_MOUSEHWHEEL:
+                    int delta = (short)HIWORD(wParam);
+                    textBox_OnMouseHWheel(delta);
+                    handled = true;
+                    return (IntPtr)1;
+            }
+            return IntPtr.Zero;
+        }
+
+        /// <summary>
+        /// Gets high bits values of the pointer.
+        /// </summary>
+        private static int HIWORD(IntPtr ptr)
+        {
+            var val32 = ptr.ToInt32();
+            return ((val32 >> 16) & 0xFFFF);
+        }
+    }
 }
